b1689624304fcebcbccf3e6bf7e05c24e2fa8f16
mukhtaar.maxamed <mukhtaarmaxamed682@gmail.com>
2023-12-02 19:46:26 -0800

    now

---
+++ Desktop/All-tast/web-dev-tests/idex.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+function sum() {
+
+$x = 4;
+$y = 5;
+$t = $x + $y;
+
+echo $t ;
+
+}
+function sap() {
+
+    $x = 8;
+    $y = 6;
+    $d = $x - $y;
+    
+    echo $d ;
+        
+        }
+
+
+    function mult() {
+
+        $x = 7;
+        $y = 9;
+        $s = $x * $y;
+        
+        echo $s ;
+
+              }
+
+    ?>
+</body>
+</html>
